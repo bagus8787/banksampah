f495c04fd6a1b67128fc7f3a0085822e6aee4428
Bagus8787 <bagusbagus2733@gmail.com>
2020-12-15 09:25:23 +0700

    Perbaikan ukuran foto profil

--- app/src/main/java/com/sahitya/banksampahsahitya/base/fragment/ProfileFragment.java
+++ app/src/main/java/com/sahitya/banksampahsahitya/base/fragment/ProfileFragment.java
@@ -50,7 +50,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
